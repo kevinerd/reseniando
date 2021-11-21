dcb5aefa7219ac7007a47eab627e33f6337d63d0
Kevin Firmani <kevinjf2011@gmail.com>
2021-11-20 23:38:33 -0300

    Cambios menores

--- src/main/java/com/reseniando/grupo4/enumeraciones/Generos.java
+++ src/main/java/com/reseniando/grupo4/enumeraciones/Generos.java
@@ -1,7 +1,10 @@
 package com.reseniando.grupo4.enumeraciones;
 
 public enum Generos {
-    TERROR("Terror"), COMEDIA("Comedia"), CIENCIA_FICCION("Ciencia Ficción"), ROMANCE("Romance"), INFANTIL("Infantil"), MISTERIO("Misterio"), POLICIAL("Policial"), AUTOAYUDA("Autoayuda"), FANTASÍA("Fantasía"), ADOLESCENTES("Adolescentes"), FILOSOFÍA("Filosofía");
+    TERROR("Terror"), COMEDIA("Comedia"), CIENCIA_FICCION("Ciencia Ficción"), ROMANCE("Romance"), 
+    INFANTIL("Infantil"), MISTERIO("Misterio"), POLICIAL("Policial"), AUTOAYUDA("Autoayuda"), 
+    FANTASÍA("Fantasía"), ADOLESCENTES("Adolescentes"), FILOSOFÍA("Filosofía"), NOVELA("Novela"),
+    FICCION("Ficción");
 
     private final String gen;
 
